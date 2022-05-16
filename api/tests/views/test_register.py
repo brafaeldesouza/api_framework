c40b68471ccce2259fe26decb8aea392bd50f927
brafaeldesouza <brafaeldesouza@gmail.com>
2022-05-16 15:53:21 -0300

    feat: add create log test

--- api/tests/views/test_register.py
+++ api/tests/views/test_register.py
@@ -1,6 +1,7 @@
 import json
 from django.shortcuts import resolve_url as r
 from django.urls import reverse
+from api.models import Log
 
 from api.tests.base import LoggedInTestCaseMixin
 
@@ -17,4 +18,11 @@ class RegisterAPIViewTest(LoggedInTestCaseMixin):
         self.assertEqual(data['data'][0]['title'], "delectus aut autem")
         self.assertEqual(data['data'][3]['title'], "et porro tempora")
         
+
+    def test_create_log(self):
+        response = self.client.get(reverse('api:list_register', kwargs={}), format="json")
+        data     = json.loads(response.content)
         
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['data'][0]['title'], "delectus aut autem")
+        self.assertTrue(Log.objects.exists())
