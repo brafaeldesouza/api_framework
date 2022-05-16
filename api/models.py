459a687f92f9f370c0331606ac2efbfe5ee9d82e
brafaeldesouza <brafaeldesouza@gmail.com>
2022-05-16 12:03:04 -0300

    feat: add Register model

--- api/models.py
+++ api/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Register(models.Model):
+    """ Register model used to store all registers of service """
+    
+    user      = models.ForeignKey(User, on_delete=models.CASCADE)
+    title     = models.CharField(max_length=100)
+    completed = models.BooleanField()
+    
+    def __str__(self):
+        return f"{self.id} | {self.user}: {self.title} - {self.completed}"
