7d72d1b977e844352d86db7d0904831a4ae36b81
brafaeldesouza <brafaeldesouza@gmail.com>
2022-05-16 15:49:29 -0300

    feat: add Log model

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+
+from api.models import Log
+
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'user', 'timestamp')
--- api/models.py
+++ api/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from jsonfield import JSONField
 
+class Log(models.Model):
+    """ Register model used to store all registers of service """
+    
+    timestamp      = models.DateTimeField(default=timezone.now, editable=False)
+    data           = JSONField(default=dict)
+    status_code    = models.CharField(max_length=3)
+    status_message = models.CharField(max_length=150)
+    user           = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.timestamp} | {self.user}: {self.status_code}"
 
 class Register(models.Model):
     """ Register model used to store all registers of service """
